14f1156b0d8b605404798cf3fc952b6f1f15d46e
Mashhur <mashhur.sattorov@gmail.com>
2019-02-01 11:06:31 +0900

    Project setup

---
+++ src/main/java/com/elastic/query/builder/esquerybuilder/EsQueryBuilderApplication.java
@@ -0,0 +1,14 @@
+package com.elastic.query.builder.esquerybuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsQueryBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsQueryBuilderApplication.class, args);
+	}
+
+}
+
