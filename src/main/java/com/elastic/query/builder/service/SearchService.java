24e0459d01780331184bebcab2d8dda4d79ef162
Mashhur <mashhur.sattorov@gmail.com>
2020-03-31 16:32:44 +0900

    Test code added.

--- src/main/java/com/elastic/query/builder/service/SearchService.java
+++ src/main/java/com/elastic/query/builder/service/SearchService.java
@@ -1,6 +1,7 @@
 package com.elastic.query.builder.service;
 
 import com.elastic.query.builder.common.Constants;
+import com.elastic.query.builder.engine.model.EsBoolType;
 import com.elastic.query.builder.engine.model.EsQuery;
 import com.elastic.query.builder.engine.model.EsQueryType;
 import com.elastic.query.builder.entity.EsItem;
@@ -15,7 +16,9 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class SearchService {
@@ -56,5 +59,41 @@ public class SearchService {
         query.addQueryAttribute("name", name);
 
         return query;
+    }
+
+    // Answer for following question with My ES Query Builder
+    // https://stackoverflow.com/questions/60940757/elasticsearch-java-query-with-combination-of-and-or
+    private void test() {
+        EsQuery query = new EsQuery();
+
+        EsQuery mainMatchQuery = new EsQuery();
+        mainMatchQuery.setQueryType(EsQueryType.MATCH);
+        mainMatchQuery.addQueryAttribute("isActive", 1);
+
+        EsQuery orQuery = new EsQuery();
+        orQuery.setBoolType(EsBoolType.SHOULD);
+        Map<Integer, String[]> termsMap = new HashMap<>(); // suppose it has a value
+        for (Map.Entry<Integer, String[]> entry : termsMap.entrySet()) {
+            int key = entry.getKey();
+            String[] value = entry.getValue();
+
+
+            EsQuery matchQuery = new EsQuery();
+            matchQuery.setQueryType(EsQueryType.MATCH);
+            matchQuery.addQueryAttribute("storeCode", key);
+
+            EsQuery termsQuery = new EsQuery();
+            termsQuery.setQueryType(EsQueryType.TERMS);
+            termsQuery.addQueryAttribute("storeGenre", value);
+            orQuery.addQuery(matchQuery);
+            orQuery.addQuery(termsQuery);
+        }
+
+        mainMatchQuery.addQuery(orQuery);
+
+        SearchHit[] searchHits = searchRepository.search(Constants.ES_INDEX_NAME, Constants.ES_INDEX_TYPE, query);
+        for (SearchHit hit : searchHits) {
+            // searched item(s) can be accessed here
+        }
     }
 }
